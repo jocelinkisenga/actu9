29ab61be093bd6f98982604d72bfaa52583f59d8
kisenga shuka jocelin <72705409+jocelinkisenga@users.noreply.github.com>
2025-02-21 14:03:23 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,"index"])->name("home");
 Route::get("/live",[HomeController::class,"live"])->name("live");
-Route::get("article/{id}",[PostController::class,"show"])->name("posts.show");
+Route::get("article/{title}/{id}",[PostController::class,"show"])->name("posts.show");
 Route::get("couponsHome/",[CouponController::class,"index"])->name("coupons.index");
 Route::get("frontPosts",[PostController::class,"front"])->name("posts.front");
 
